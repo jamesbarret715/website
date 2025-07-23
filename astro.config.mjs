8552647e41a7986684129724e72f646e322aa0ab
James Barret <jamesbarret715@gmail.com>
2025-07-23 15:50:12 +0100

    add robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vue from '@astrojs/vue';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [vue()]
+  integrations: [vue(), sitemap()]
 });
